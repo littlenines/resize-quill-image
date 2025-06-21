daa59d1930192bf499c5aebd2ebac5d078e3ad1d
littlenines <nemanja584@hotmail.com>
2025-06-22 00:03:33 +0200

    fix import

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname, resolve } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'resize-quill-image': resolve(__dirname, '../src/ImageResize.js'),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     'resize-quill-image': resolve(__dirname, '../src/ImageResize.js'),
+  //   },
+  // },
   build: {
   rollupOptions: {
     output: {
